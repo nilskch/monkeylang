33c36320171cdea8441ba266d8e9bb6f7984b962
Nils Koch <nkoch@palantir.com>
2024-07-01 11:30:39 +0100

    remove target dir

--- target/debug/deps/compiler-fa79ce9f5d0b790c.d
+++
@@ -1,7 +0,0 @@
-/Volumes/git/learning/monkeylang-rust/target/debug/deps/libcompiler-fa79ce9f5d0b790c.rmeta: compiler/src/lib.rs compiler/src/lexer.rs compiler/src/token.rs
-
-/Volumes/git/learning/monkeylang-rust/target/debug/deps/compiler-fa79ce9f5d0b790c.d: compiler/src/lib.rs compiler/src/lexer.rs compiler/src/token.rs
-
-compiler/src/lib.rs:
-compiler/src/lexer.rs:
-compiler/src/token.rs:
