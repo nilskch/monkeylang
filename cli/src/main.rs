38381a43a57961eb1e5701fe3eace41b53d415f1
Nils Koch <nkoch@palantir.com>
2024-07-02 13:28:11 +0100

    add cli dependencies

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,22 @@
-use compiler::repl;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
-    // implement cli here
-    repl::start();
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 }
