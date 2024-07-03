fcf5bc810c5237ded924bad7dc1fb376f0bb201f
Nils Koch <nkoch@palantir.com>
2024-07-03 15:16:36 +0100

    improve monkey cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,8 +4,9 @@ fn main() {
     let _m = Command::new("My Program")
         .author("Nils Koch, mail@nilskch.dev")
         .version("1.0.2")
-        .about("Explains in brief what the program does")
-        .arg(Arg::new("in_file"))
+        .about("Use the monkey cli to run, build, or format monkey code!")
+        .arg(Arg::new("command").help("'run', 'build', 'fmt'."))
+        .arg(Arg::new("file").help("The path to the .mky file."))
         .after_help(
             "Longer explanation to appear after the options when \
                      displaying the help information from --help or -h",
