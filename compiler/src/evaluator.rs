f2feeb01f799b549b5db78b5df508e2b160aed9e
nilskch <mail@nilskch.dev>
2024-07-11 10:53:07 +0200

    add evaluating integers

--- compiler/src/evaluator.rs
+++ compiler/src/evaluator.rs
@@ -1,8 +1,38 @@
+use crate::ast::expression::Expression;
+use crate::ast::statement::Statement;
 use crate::ast::Node;
 use crate::object::Object;
 
 pub fn eval(node: Node) -> Object {
-    Object::Null
+    match node {
+        Node::Program(program) => eval_statements(program.statements),
+        Node::Expression(expr) => eval_expression(expr),
+        Node::Statement(stmt) => eval_statement(stmt),
+    }
+}
+
+fn eval_statements(stmts: Vec<Statement>) -> Object {
+    let mut result = Object::Null;
+
+    for stmt in stmts {
+        result = eval(Node::Statement(stmt));
+    }
+
+    result
+}
+
+fn eval_statement(stmt: Statement) -> Object {
+    match stmt {
+        Statement::Expr(expr_stmt) => eval_expression(expr_stmt.expression),
+        _ => unreachable!(),
+    }
+}
+
+fn eval_expression(expr: Expression) -> Object {
+    match expr {
+        Expression::Integer(integer_literal) => Object::Integer(integer_literal.value),
+        _ => unreachable!(),
+    }
 }
 
 #[cfg(test)]
@@ -29,6 +59,15 @@ mod tests {
     }
 
     fn test_integer_object(object: Object, expected: i64) {
-        // let integer_obj = match
+        let integer_value = match object {
+            Object::Integer(integer_value) => integer_value,
+            _ => unreachable!(),
+        };
+
+        assert_eq!(
+            integer_value, expected,
+            "object has wrong value. got='{}'. wanted='{}'",
+            integer_value, expected
+        );
     }
 }
