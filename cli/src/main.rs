d59b6514c762d2f0ba5682bf051d543efa778808
Nils Koch <nkoch@palantir.com>
2024-07-29 16:29:24 +0100

    test actions

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,18 +1,35 @@
-use clap::{Arg, Command};
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
-    let cli = Command::new("monkey")
+    let matches = Command::new("monkey")
         .author("Nils Koch, mail@nilskch.dev")
         .version("1.0.2")
         .about("Use the monkey cli to run or format monkey code!")
-        .arg(Arg::new("command").help("'run' or 'fmt'."))
-        .arg(Arg::new("file").help("The path to the .mky file."))
+        .arg(
+            Arg::new("cmd")
+                .help("'run' or 'fmt'.")
+                .action(ArgAction::Set),
+        )
+        .arg(
+            Arg::new("file")
+                .help("Path to the .mky file.")
+                .action(ArgAction::Set),
+        )
         .get_matches();
 
-    if !cli.args_present() {
+    if !matches.args_present() {
         println!("Welcome to the Monkey Programming Language!");
         return compiler::repl::start();
     }
 
-    // TODO: handle 'run' and 'fmt' subcommands
+    let _file_path = match matches.get_one::<&str>("file") {
+        Some(file_path) => *file_path,
+        None => panic!("No file path provided."),
+    };
+
+    match *matches.get_one::<&str>("cmd").unwrap() {
+        "run" => println!("RUN"),
+        "fmt" => println!("FMT"),
+        _ => println!("ERROR"),
+    }
 }
